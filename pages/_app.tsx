c4c8593ca67f50238fb60fe12600dc7f7c9ae8be
Laurie Roy <laurieroy.dev@gmail.com>
2023-01-05 13:25:33 -0800

    Add redux, setup store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import { Provider } from 'react-redux';
+import store from "../store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from 'redux-persist';
+
 import type { AppProps } from 'next/app'
 
+let persistor = persistStore(store);
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
