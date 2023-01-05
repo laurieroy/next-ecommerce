da8201a6439c89971845852018808fbba2af6ce1
Laurie Roy <laurieroy.dev@gmail.com>
2023-01-05 12:49:35 -0800

    Add mongoDB, connections

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import db from "../../utils/db";
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -9,5 +10,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  db.connectDb();
+  db.disconnectDb();
   res.status(200).json({ name: 'John Doe' })
 }
